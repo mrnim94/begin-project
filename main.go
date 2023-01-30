4c706ac87b4ae5940acb03df935ac8421795bf21
MR Nim <mr.nim94@gmail.com>
2023-01-30 08:51:43 +0000

    add them init tren main de tranh loi log

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"begin-project/log"
 )
+
+func init() {
+	os.Setenv("APP_NAME", "XXXX")
+	log.InitLogger(false)
+	os.Setenv("TZ", "Asia/Ho_Chi_Minh")
+}
 
 func main() {
 	e := echo.New()
